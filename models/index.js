c2884cfd5379f4167f086f207dedf2051d19ce35
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-11-07 19:22:37 -0800

    added in the code for models index.js

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require("./user");
+const BlogPost = require("./blogPost");
+const Comment = require("./comment");
+
+User.hasMany(BlogPost, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+});
+
+BlogPost.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+})
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+Comment.belongsTo(BlogPost, {
+    foreignKey: "blogPost_id",
+    onDelete: "CASCADE",
+});
+
+BlogPost.hasMany(Comment, {
+    foreignKey: "blogPost_id",
+    onDelete: "CASCADE",
+});
+
+module.exports = { User, BlogPost, Comment };
